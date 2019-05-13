b0b5da753bcb4aeb54122c8f7637122eb788023e
Geno Diaz <genodiaz@yahoo.com>
2019-05-12 17:33:00 -0700

    User sign up page, user query, & User reusable component

--- components/Nav.js
+++ components/Nav.js
@@ -1,8 +1,15 @@
 import Link from 'next/link';
 import NavStyles from './styles/NavStyles';
+import User from './User';
 
 const Nav = () => (
   <NavStyles>
+    <User>
+      {({ data: { me } }) => {
+        if (me) return <div>{me.name}</div>
+        return null;
+      }}
+    </User>
     <Link href="/items">
       <a>Shop</a>
     </Link>
